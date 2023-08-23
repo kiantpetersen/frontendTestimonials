ebd37a9aa93335b9ee11f2ddd1b4db7850780602
unknown <kiantpetersen@gmail.com>
2023-08-24 00:59:28 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { testimonialData } from './assets/testimonialData'
+import TestimonialCard from './TestimonialCard'
+function App() {
+  const [data, setData] = useState(testimonialData)
+
+  return (
+    <div className='testimonial-section'>
+      <h1>Testimonial Section</h1>
+      <div className='testimonial-grid'>
+
+        {/* </div> */}
+        <TestimonialCard testimonialData={data[0]} />
+        <TestimonialCard testimonialData={data[1]} />
+        <TestimonialCard testimonialData={data[4]} />
+        <TestimonialCard testimonialData={data[2]} />
+        <TestimonialCard testimonialData={data[3]} />
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/TestimonialCard.jsx
@@ -0,0 +1,21 @@
+
+
+function TestimonialCard({ testimonialData }) {
+    return <div className='testimonial-card'>
+        <div className='testimonial-header'>
+            <div className='testimonial-img-container'>
+                <img className='testimonial-profile-img' src={testimonialData.image} alt='testimonial_profile_image' />
+            </div>
+            <div className='testimonial-header-textbox'>
+                <p className='testimonial-author testimonial-header-text'>{testimonialData.name}</p>
+                <p className='testimonial-header-text'>Verified Graduate</p>
+            </div>
+        </div>
+        <div>
+            <p className='testimonial-title'>{testimonialData.title}</p>
+            <p className='testimonial-content'>{testimonialData.content}</p>
+        </div>
+    </div>
+}
+
+export default TestimonialCard
